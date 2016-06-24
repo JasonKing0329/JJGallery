26d58abf06883210ce7a80c37040ad217289d436
Your Name <you@example.com>
2016-06-24 15:56:50 +0800

    add targetapi define

--- app/src/main/java/com/jing/app/jjgallery/viewsystem/main/settings/SettingsActivity.java
+++ app/src/main/java/com/jing/app/jjgallery/viewsystem/main/settings/SettingsActivity.java
@@ -207,6 +207,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      * This method stops fragment injection in malicious applications.
      * Make sure to deny any unknown fragments here.
      */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     protected boolean isValidFragment(String fragmentName) {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
